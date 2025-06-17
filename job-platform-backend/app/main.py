a39fcab253877c5ace8666b38dd99ca6c1e66075
Michael Eliyahu <mmyy605@gmail.com>
2025-06-17 10:36:44 +0300

    add middlware

--- job-platform-backend/app/main.py
+++ job-platform-backend/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.routers import user_router, company_router, application_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # כתובת ה-frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router.router)
 app.include_router(company_router.router)
