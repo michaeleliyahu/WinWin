0aa0b0773a6a15ff0a0bf58e11e15cbbd1a2f068
Michael Eliyahu <mmyy605@gmail.com>
2025-07-31 13:42:29 +0300

    add insert

--- job-platform-backend/app/routers/application_router.py
+++ job-platform-backend/app/routers/application_router.py
@@ -1,13 +1,21 @@
 from fastapi import Depends, UploadFile, File, APIRouter, status
-from fastapi.responses import Response
+from fastapi.responses import StreamingResponse, Response
 from app.schemas.application_schema import ApplicationForm
+from motor.motor_asyncio import  AsyncIOMotorGridFSBucket
+from app.services.application_service import insert_application
+from app.db import db
+
+fs = AsyncIOMotorGridFSBucket(db)
+
 router = APIRouter(prefix="/application", tags=["application"])
 
 @router.post("/", status_code=status.HTTP_204_NO_CONTENT)
-async def create_application(
-    form: ApplicationForm = Depends(), 
-    resume: UploadFile = File(...) 
-):
-    
-
+async def create_application(form: ApplicationForm = Depends(), resume: UploadFile = File(...)):
+    await insert_application(form, resume)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.get("/resume/{file_id}")
+async def download_resume(file_id: str):
+    grid_out = await fs.open_download_stream(file_id)
+    return StreamingResponse(grid_out, media_type="application/octet-stream")
--- job-platform-backend/app/services/application_service.py
+++ job-platform-backend/app/services/application_service.py
@@ -1,4 +1,32 @@
 from app.db import db
 from app.schemas.application_schema import ApplicationForm
+from fastapi import UploadFile
+from datetime import datetime
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
 
-companies_collection = db["company"]
+fs = AsyncIOMotorGridFSBucket(db)
+
+applications_collection = db["applications"]
+
+async def insert_application(form: ApplicationForm, resume: UploadFile):
+    # שמירת הקובץ ב-GridFS
+    file_id = await fs.upload_from_stream(
+        resume.filename,
+        resume.file  # resume.file הוא ה-BufferedReader של UploadFile
+    )
+
+    document = {
+        "firstName": form.firstName,
+        "lastName": form.lastName,
+        "mobileNumber": form.mobileNumber,
+        "email": form.email,
+        "jobLink": form.jobLink,
+        "companyId": form.companyId,
+        "userId": form.userId,
+        "resume_file_id": file_id,  # שומר את מזהה הקובץ ב-GridFS
+        "uploaded_at": datetime.utcnow()
+    }
+
+    await applications_collection.insert_one(document)
+
+
